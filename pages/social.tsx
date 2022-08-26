14caaadce88d464c5d05c9268de3041f5b67c979
AndreyB-FE <andrei.228.bogushevich@gmail.com>
2022-08-26 14:17:06 +0300

    initializing project

---
+++ pages/social.tsx
@@ -0,0 +1,10 @@
+import type { FC } from 'react';
+
+interface socialProps {}
+
+const social: FC<socialProps> = ({}) => {
+        return (
+            <div>hello</div>
+        );
+}
+export default social;
