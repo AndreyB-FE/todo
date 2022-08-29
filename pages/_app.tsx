408a82d8e5cf7bbfbd1550e01e9f9b4e0a941f68
AndreyB-FE <andrei.228.bogushevich@gmail.com>
2022-08-29 15:14:13 +0300

    fix: checked issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const Global = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`;
+
+const theme = {
+  media: {
+    phone: "(max-width: 452px)",
+    tablet: "(max-width:768px) and (min-width: 452px)",
+  },
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    // <>
+    //   <ThemeProvider theme={theme}>
+    //     <Global></Global>
+    <Component {...pageProps} />
+    /* </ThemeProvider>
+    </> */
+  );
 }
+
+//https://nextjs.org/docs/basic-features/fast-refresh#how-it-works read this topic
 
 export default MyApp;
